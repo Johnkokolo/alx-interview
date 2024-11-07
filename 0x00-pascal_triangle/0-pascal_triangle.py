41b877e7b6f21af42340fb989a42e9a2381fb215
john kokolo <johnkokolo@rocketmail.com>
2024-11-07 22:14:48 +0000

    task 1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing Pascal's Triangle of size n.
